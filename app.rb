6090711907bc49a7884727d20a138776e17d2183
Sharmine <sharmine.ss@gmail.com>
2023-05-02 13:43:45 +0100

    completed challenge in building routes

--- app.rb
+++ app.rb
@@ -7,4 +7,36 @@ class Application < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
   end
+
+  # GET /
+  # Root path (homepage, index page)
+  get '/' do
+    return 'Hello!'
+  end
+
+  get '/posts' do
+    name = params[:name] # access passed parameters
+    cohort_name = params[:cohort_name]
+
+    return "Hello #{name}, you are in the cohort #{cohort_name}"
+  end
+
+  get "/hello" do
+    name = params[:name]
+
+    return "Hello #{name}"
+  end
+
+  post '/posts' do
+    title = params[:title]
+    
+    return "Post was created with title: #{title}"
+  end
+
+  post "/submit" do
+    name = params[:name]
+    message = params[:message]
+
+    return "Thanks #{name}, you sent this message: '#{message}'"
+  end
 end
